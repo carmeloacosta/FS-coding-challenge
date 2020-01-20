428eb4613865324c928b8457f69afa8a2de64f25
Carmelo <carmelo.acosta@sage.com>
2020-01-20 20:55:35 +0100

    Complete model coverage

--- yoga/yoga/tests/tests_models.py
+++ yoga/yoga/tests/tests_models.py
@@ -2,7 +2,6 @@
 
 
 import os
-from json import dumps
 from django.test import TestCase
 
 from ..models import Posture, User
@@ -15,7 +14,59 @@ class ModelsTestCase(TestCase):
     fixtures = [os.path.join(FIXTURE_DIRS[0], 'initial_state.json'), ]
 
     def setUp(self):
-        pass
+        self.user_name = "albertmiro"
+        self.user = User.objects.get(name=self.user_name)
+
+    def test__get_all_users__ok(self):
+
+        # Test main
+        uqs = User.objects.all()
+
+        # Check results
+        expected_user_name = self.user_name
+        expected_email = "albert.miro@yogasolo.app"
+        expected_password = "albertmiro2020"
+
+        self.assertEqual(len(uqs), 1)
+        self.assertEqual(uqs[0].name, expected_user_name)
+        self.assertEqual(uqs[0].email, expected_email)
+        self.assertEqual(uqs[0].password, expected_password)
+
+    def test__get_all_postures__ok(self):
+
+        # Test main
+        pqs = Posture.objects.all()
+
+        # Check results
+        expected_posture_list = [
+            {
+                "id": "5df111bd23f72ffeefe0fa2f",
+                "name": "Initial posture 1",
+                "description": "Initial posture 1 description",
+                "picture": "https://loremflickr.com/320/320/yoga,asana"
+             },
+            {
+                "id": "5df111bd23f72ffeefe0fa30",
+                "name": "Initial posture 2",
+                "description": "Initial posture 2 description",
+                "picture": "https://loremflickr.com/320/320/yoga,asana"
+            }
+        ]
+
+        self.assertEqual(len(pqs), 2)
+        for posture in pqs:
+            found = False
+            for posture_info in expected_posture_list:
+                if posture_info["id"] == posture.id and \
+                        posture_info["name"] == posture.name and \
+                        posture_info["description"] == posture.description and \
+                        posture_info["picture"] == posture.picture and \
+                        posture.user == self.user:
+
+                    found = True
+                    break
+
+            self.assertEqual(found, True)
 
     def test__new_user__ok(self):
 
