acc928bdb1c4a4c27a3ec057eb1c0fbe0b16145e
Carmelo <carmelo.acosta@sage.com>
2020-01-21 11:32:04 +0100

    Added add_user to Controller

--- yoga/yoga/controller.py
+++ yoga/yoga/controller.py
@@ -10,6 +10,10 @@ class Controller():
     """
         Implements the Yoga bussiness logic. It access the underlying data base system in order to persist the
         required data.
+
+        IMPLEMENTATION NOTE: IN ORDER TO KEEP THIS TECHNICAL CHALLENGE SIMPLE I WILL NOT ADD ALL CRUD FOR BOTH
+        USERS AND POSTURES (I.E., CREATE/READ/UPDATE/DELETE), JUST THE MINIMAL OPERATIONS ASKED IN THE TEST. IN A REAL
+        IMPLEMENTATION IT WILL BE NEEDED TO IMPLEMENT IT ALL.
     """
     def __init__(self, elogger=logger):
         self.logger = elogger
@@ -35,5 +39,45 @@ class Controller():
             result.append(posture.to_dict())
 
         self.logger.debug("user_only: {}, result: {}".format(user_only, result))
+
+        return result
+
+    def add_user(self, user_info):
+        """
+            Adds a new user to the system.
+
+        :param user_info: (dict) Info of the user to be added. Follows the following format:
+
+                {
+                    "name" : <user_name>
+                    "email": <email>
+                    "password": <password>
+                }
+
+                with:
+
+                    <user_name> : (str) User name
+                    <email> : (str) Email address
+                    <password> : (str) Password
+
+        :return: (bool) True if the user was properly added. If there is another user with the same name it returns
+            False, and the user is not added.
+        """
+        result = False
+        try:
+            user = User.objects.get(name=user_info["name"])
+            self.logger.debug("Trying to add already existent user {}".format(user.name))
+
+        except KeyError:
+            # Bad input
+            self.logger.error("Error while adding new user: missing field 'name' in user_info={}".format(user_info))
+
+        except User.DoesNotExist:
+            # OK, it does not exists. Add it
+            new_user = User(**user_info)
+            new_user.save()
+
+            result = True
+            self.logger.info("Added new user {}".format(user_info["name"]))
 
         return result
--- yoga/yoga/tests/tests_controller.py
+++ yoga/yoga/tests/tests_controller.py
@@ -16,16 +16,16 @@ class ControllerTestCase(TestCase):
     fixtures = [os.path.join(FIXTURE_DIRS[0], 'initial_state.json'), ]
 
     def setUp(self):
+        self.logger = MagicMock()
+        self.controller = Controller(self.logger)
+
         self.user_name = "albertmiro"
         self.user = User.objects.get(name=self.user_name)
 
     def test__get_all_postures__ok(self):
 
-        logger = MagicMock()
-        controller = Controller(logger)
-
         # Test main
-        postures = controller.get_all_postures()
+        postures = self.controller.get_all_postures()
 
         # Check results
         expected_posture_list = [
@@ -57,11 +57,8 @@ class ControllerTestCase(TestCase):
 
     def test__get_user_postures__ok(self):
 
-        logger = MagicMock()
-        controller = Controller(logger)
-
         # Test main
-        postures = controller.get_all_postures(self.user_name)
+        postures = self.controller.get_all_postures(self.user_name)
 
         # Check results
         expected_posture_list = [
@@ -85,9 +82,45 @@ class ControllerTestCase(TestCase):
 
     def test__get_all_postures__wrong(self):
 
-        logger = MagicMock()
-        controller = Controller(logger)
+        # Test main
+        postures = self.controller.get_all_postures("unknown_user")
+        self.assertEqual(len(postures), 0)
+
+    def test__add_user__ok(self):
+
+        user_info = {
+            "name": "carmeloacosta",
+            "email": "carmelo.acosta@gmail.com",
+            "password": "carmeloacosta2020",
+        }
 
         # Test main
-        postures = controller.get_all_postures("unknown_user")
-        self.assertEqual(len(postures), 0)
+        result = self.controller.add_user(user_info)
+
+        # Check results
+        self.assertEqual(result, True)
+
+        user = User.objects.get(name=user_info["name"])
+        self.assertEqual(user.name, user_info["name"])
+        self.assertEqual(user.email, user_info["email"])
+        self.assertEqual(user.password, user_info["password"])
+
+    def test__add_user__wrong(self):
+
+        user_info_list = [{
+                "name": "albertmiro",
+                "email": "albert.miro@yogasolo.app",
+                "password": "albertmiro2020",
+            },
+            {
+                "email": "carmelo.acosta@gmail.com",
+                "password": "carmeloacosta2020",
+            }
+        ]
+
+        # Test main
+        for user_info in user_info_list:
+            result = self.controller.add_user(user_info)
+
+            # Check results
+            self.assertEqual(result, False)
